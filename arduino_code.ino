955ed0c61afce7eb5c0ede7d7d31d7b50ebbc753
thummalavishnupriya <51885136+thummalavishnupriya@users.noreply.github.com>
2024-01-30 23:53:29 +0530

    Add files via upload

---
+++ arduino_code.ino
@@ -0,0 +1,19 @@
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(0,1);
+void setup(){
+  mySerial.begin(9600);
+  Serial.begin(9600);
+  delay(100);
+}
+
+// Arduino Uno code to receive the message
+void loop(){
+  if(mySerial.available()){
+    String received = "";
+    received = Serial.readStringUntil('\n');
+    Serial.println(received);
+  }
+  String mssage = "Hallo from arduino";
+  Serial.println(mssage);
+  delay(2000);
+}
